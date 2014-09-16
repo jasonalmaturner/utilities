bff4921b66eec99c5cbaa1011a05ac1653a12c81
jasonalmaturner <jasonalmaturner@gmail.com>
2014-09-16 15:42:02 -0600

    Left off at the each/iterator exercise

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -16,16 +16,41 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    if (n === undefined){
+      return array[0];
+    } 
+    else {
+      return array.slice(0, n);
+    }
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined){
+      return array[array.length - 1];
+    } 
+    else if(n === 0){
+      return [];
+    }
+    else {
+      return array.slice(-n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection) === true){
+      for(var i = 0; i < collection.length; i++){
+        var key = i;
+        var value = collection[i];
+        iterator(key, value, collection);
+      }
+    }
+    else{
+      for(var key in collection){}
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
